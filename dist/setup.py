f2062647b4b996f52be323fdd8851fbaffa51f9b
David Halls <dahalls@gmail.com>
2013-10-05 20:59:52 +0100

    Don't need custom monkey patching in setup.py - using fork of bento with them in

--- dist/setup.py
+++ dist/setup.py
@@ -5,40 +5,5 @@ monkey_patch()
 
 from setuptools import setup
 
-import types
-from os import path
-import shutil
-import bento
-
-# copy .egg-info to install directory and write it to install record
-
-_orig_resolve_paths_with_destdir = bento.installed_package_description.BuildManifest.resolve_paths_with_destdir
-
-_src = None
-_dest = None
-
-def resolve_paths_with_destdir(self, src_root_node):
-    global _src, _dest
-    _src = path.join('pip-egg-info', self.meta['name'] + '.egg-info')
-    _dest = path.abspath(self.resolve_path(path.join('$sitedir', self.meta['name'] + '-' + self.meta['version'] + '-py$py_version_short.egg-info')))
-    return _orig_resolve_paths_with_destdir(self, src_root_node)
-
-bento.installed_package_description.BuildManifest.resolve_paths_with_destdir = resolve_paths_with_destdir
-
-_orig_safe_write = bento.utils.io2.safe_write
-
-def _safe_write(target, writer, mode="wb"):
-    if target.endswith('install-record.txt'):
-        _orig_writer = writer
-        def _writer(fid):
-            r = _orig_writer(fid)
-            shutil.copytree(_src, _dest)
-            fid.write(_dest + "\n")
-            return r
-        writer = _writer
-    return _orig_safe_write(target, writer, mode)
-
-bento.utils.io2.safe_write = _safe_write
-
 if __name__ == '__main__':
     setup()
