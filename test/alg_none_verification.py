bcc2232826e96adb7fa109ce32d0dcc1b2c2645d
David Halls <dahalls@gmail.com>
2015-02-18 07:34:31 +0000

    Forgot to add additional test for alg none behaviour

---
+++ test/alg_none_verification.py
@@ -0,0 +1,36 @@
+""" Test verification of tokens with algorithms none """
+
+from pyvows import Vows, expect
+import jwt
+
+# JWT from @timmclean
+jwt_alg_none = "eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpYXQiOjAsIm5iZiI6MCwiZXhwIjoxZTIwfQ."
+
+@Vows.batch
+class AlgNoneVerification(Vows.Context):
+    """ Check we get an error when verifying token that has alg none with
+        a public key """
+    def topic(self):
+        """ Return the token """
+        return jwt_alg_none
+
+    class VerifyJWT(Vows.Context):
+        """ Verify token without specifying public key """
+        def topic(self, topic):
+            """ Verify the token """
+            return jwt.verify_jwt(topic, None)
+
+        def token_should_verify(self, r):
+            """ Should verify and match expected claims """
+            expect(r).to_be_instance_of(tuple)
+
+    class VerifyJWTWithPublicKey(Vows.Context):
+        """ Verify token with public key """
+        def topic(self, topic):
+            """ Verify the token with some public key """
+            return jwt.verify_jwt(topic, 'anysecrethere')
+
+        def token_should_fail_to_verify_when_pub_key_specified(self, r):
+            """ Check it doesn't verify because key specified by alg is none """
+            expect(r).to_be_an_error()
+            expect(str(r)).to_equal('key specified but alg is none')
