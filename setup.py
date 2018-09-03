a626d681507bf7534e892fe78c3ce58871f4fb22
David Halls <dahalls@gmail.com>
2018-09-03 20:07:29 +0100

    Rev version, add 3.4 to tested versions

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(name):
 
 setup(
     name='python_jwt',
-    version='3.1.0',
+    version='3.2.0',
     description="Module for generating and verifying JSON Web Tokens",
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
