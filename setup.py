fab0400a6cb887eae19ee7f510d1dcf3758c0323
David Halls <dahalls@gmail.com>
2019-11-14 22:17:58 +0000

    Fix Travis test fail (update Node version)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(name):
 
 setup(
     name='python_jwt',
-    version='3.2.4',
+    version='3.2.5',
     description="Module for generating and verifying JSON Web Tokens",
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
